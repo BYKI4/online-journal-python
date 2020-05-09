99c72d21917039ac377109957b3a7a3b6fb77628
BYKI4 <64970557+BYKI4@users.noreply.github.com>
2020-05-09 20:18:42 +0300

    Add files via upload

---
+++ slider.py
@@ -0,0 +1,127 @@
+from flask import Flask
+from flask import url_for, request
+from flask import render_template
+from flask_login import LoginManager, login_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+from flask import redirect
+from wtforms.fields.html5 import EmailField
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:123@localhost/py_sweater'
+db = SQLAlchemy(app)
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    param = {}
+    param['username'] = "Учащийся"
+    param['title'] = 'Домашняя страница'
+    return render_template("index.html", **param)
+
+@login_manager.user_loader
+def load_user(user_id):
+    session = db_session.create_session()
+    return session.query(User).get(user_id)
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        user = session.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               Mark="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+def main():
+    app.run()
+
+from data import db_session
+db_session.global_init("db/blogs.sqlite")
+
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    name = StringField('Номер и буква класса', validators=[DataRequired()])
+    about = TextAreaField("Немного о себе")
+    submit = SubmitField('Войти')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   Mark="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   Mark="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+@app.route('/messages')
+def messages():
+    return "Здесь пока ничего нет..."
+
+@app.route('/profile')
+def profile():
+    return "Здесь пока ничего нет..."
+
+@app.route('/marks')
+def marks():
+    return "Здесь пока ничего нет..."
+
+
+@app.route('/sample_page')
+def return_sample_page():
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <title>Привет, Яндекс!</title>
+                      </head>
+                      <body>
+                        <h1>Первая HTML-страница</h1>
+                      </body>
+                    </html>"""
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
+
+
