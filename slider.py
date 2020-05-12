b3cfc7f52e004c70dbcf20c53ed91805b90cc2b2
BeyondSingularity <57673975+BeyondSingularity@users.noreply.github.com>
2020-05-12 21:44:55 +0300

    Add files via upload

--- slider.py
+++ slider.py
@@ -3,12 +3,12 @@ from flask import url_for, request
 from flask import render_template
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 from flask import redirect
 from wtforms.fields.html5 import EmailField
 from flask_sqlalchemy import SQLAlchemy
-from data.__all_models import User, Classroom, Link, Marks, GroupOfMarks
+from data.__all_models import User, Classroom, Link, Marks, GroupOfMarks, Payload
 
 
 app = Flask(__name__)
@@ -39,6 +39,8 @@ class LoginForm(FlaskForm):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect("/marks")
     form = LoginForm()
     if form.validate_on_submit():
         session = db_session.create_session()
@@ -100,23 +102,170 @@ def messages():
 
 @app.route('/profile')
 def profile():
-    return render_template('profile.html')
+    a = []
+    for clas in current_user.classrooms:
+        a.append(clas.code + ' - ' + clas.name)
+    return render_template('profile.html', title="Профиль", a=a)
 
 @app.route('/marks/<classcode>')
-@app.route('/marks')
+def marks_teacher(classcode):
+    if not current_user.is_teacher:
+        return redirect('/marks')
+    mx = 0
+    session = db_session.create_session()
+    classroom = session.query(Classroom).filter(Classroom.code == classcode).first()
+    for i in classroom.group_of_marks:
+        mx = max(mx, len(i.marks))
+    return render_template('marks_for_teacher.html', mx=mx, classcode=classcode, title='Табель успеваемости', classroom=classroom)
 
 @app.route('/marks')
 def marks():
     if current_user.is_teacher:
-        return redirect("/marks/" + current_user.)
+        for i in current_user.classrooms:
+            return redirect("/marks/" + i.code)
     mx = 0
     for i in current_user.group_of_marks:
         mx = max(mx, len(i.marks))
-    return render_template('marks.html', mx=mx)
+    return render_template('marks.html', mx=mx, title='Табель успеваемости')
 
-@app.route('/change_password')
+
+class ClassroomAddForm(FlaskForm):
+    code = StringField('Код вашего класса', validators=[DataRequired()])
+    submit = SubmitField('Добавить')
+
+
+@app.route('/add_classroom', methods=['POST', 'GET'])
+def add_classroom():
+    form = ClassroomAddForm()
+    if current_user.is_teacher:
+        return redirect('/create_classroom')
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        flag2 = False
+        for i in current_user.classrooms:
+            print(i.code)
+            if i.code == form.code.data:
+                flag2 = True
+        flag = session.query(Classroom).filter(Classroom.code == form.code.data).first()
+        print("len(form.code.data) != 5", len(form.code.data) != 5)
+        print("all([i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' for i in form.code.data])", all([i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' for i in form.code.data]))
+        print("flag", flag == None)
+        print("flag2", flag2)
+        if len(form.code.data) != 5 or not all([i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' for i in form.code.data]) or (flag == None) or flag2:
+            form.code.errors.append("Код класса введён неверно")
+            return render_template('add_classroom.html', form=form, title='Добавить класс')
+        user = session.query(User).filter(User.id == current_user.id).first()
+        classroom = session.query(Classroom).filter(Classroom.code == form.code.data).first()
+        user.classrooms.append(classroom)
+        marks = GroupOfMarks(name=current_user.name)
+        user.group_of_marks.append(marks)
+        classroom.group_of_marks.append(marks)
+        session.add(marks)
+        session.commit()
+        return render_template('your_code.html', title="Класс добавлен", insert_text="Класс был успешно добавлен")
+    return render_template('add_classroom.html', form=form, title='Добавить класс')
+
+
+class AddMarkForm(FlaskForm):
+    email = EmailField('Email ученика', validators=[DataRequired()])
+    comment = StringField('Комментарий к оценке', validators=[DataRequired()])
+    mark = IntegerField('Оценка (2, 3, 4, 5)', validators=[DataRequired()])
+    submit = SubmitField('Выставить')
+
+@app.route('/add_mark/<code>', methods=['GET', 'POST'])
+def add_mark(code):
+    form = AddMarkForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        classroom = session.query(Classroom).filter(Classroom.code == code).first()
+        flag = False
+        for user in classroom.users:
+            if user.email == form.email.data:
+                flag = True
+        if 
+    return render_template('add_mark.html', form=form, title='Выставление оценок')
+
+
+class ClassroomCreateForm(FlaskForm):
+    name = StringField('Название вашего класса', validators=[DataRequired()])
+    submit = SubmitField('Создать')
+
+
+@app.route('/your_code/<code>')
+def show_code(code):
+    return render_template('your_code.html', title='Ваш код', insert_text=f"Ваш код от нового класса: {code}. Отправьте его своим ученикам")
+
+@app.route('/create_classroom', methods=['POST', 'GET'])
+def create_classroom():
+    form = ClassroomCreateForm()
+    if not current_user.is_teacher:
+        return redirect('/add_classroom')
+    if form.validate_on_submit():
+        ans = ''
+        global step, p, MOD
+        current_hash = get_hash()
+        d = current_hash
+        current_hash += step
+        current_hash %= MOD
+        rewrite_hash(current_hash)
+        while d != 0:
+            ans += sl[d % p]
+            d //= p
+        ans += 'A' * (5 - len(ans))
+        session = db_session.create_session()
+        classroom = Classroom(name=form.name.data, code=ans)
+        user = session.query(User).filter(User.id == current_user.id).first()
+        user.classrooms.append(classroom)
+        session.add(classroom)
+        session.commit()
+        return redirect('/your_code/' + ans)
+    return render_template('create_classroom.html', title='Создать класс', form=form)
+
+
+class ChangePassword(FlaskForm):
+    old_password = EmailField('Старый пароль', validators=[DataRequired()])
+    new_password = PasswordField('Новый пароль', validators=[DataRequired()])
+    new_password_again = PasswordField('Повторите новый пароль', validators=[DataRequired()])
+    submit = SubmitField('Сменить')
+
+
+@app.route('/change_password', methods=['GET', 'POST'])
 def change_password():
-    return render_template('change_password.html')
+    form = ChangePassword()
+    if form.validate_on_submit():
+        if form.new_password.data != form.new_password_again.data:
+            form.new_password_again.errors.append("Новые пароли не совпадают")
+            return render_template('change_password.html', title='Смена пароля', form=form)
+        session = db_session.create_session()
+        if not(current_user.check_password(form.old_password.data)):
+            form.new_password_again.errors.append("Старый пароль неверен")
+            return render_template('change_password.html', title='Смена пароля', form=form)
+        user = session.query(User).filter(User.id == current_user.id).first()
+        user.set_password(form.new_password.data)
+        session.commit()
+        return redirect('/profile')
+
+    return render_template('change_password.html', title='Смена пароля', form=form)
+
+
+def get_hash():
+    session = db_session.create_session()
+    payload = session.query(Payload).filter(True).first()
+    return payload.cur_hash
+
+
+def rewrite_hash(cur_hash):
+    session = db_session.create_session()
+    payload = session.query(Payload).first()
+    payload.cur_hash = cur_hash
+    session.commit()
 
 if __name__ == '__main__':
+    p = 26
+    MOD = p ** 5
+    step = 288453275
+    s = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    sl = {}
+    for i in range(26):
+        sl[i] = s[i]
     app.run(port=8080, host='127.0.0.1')
